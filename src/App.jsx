38d2d1937237060f0d1044871374859d7bd47542
Yusuf Adegbite <yusufadegbite87@gmail.com>
2026-03-11 07:01:18 +0100

    fix portfolio filename case

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import BrandsBar from "./components/BrandsBar";
 import Services from "./components/Services";
 import Stats from "./components/Stats";
-import Portfolio from "./components/portfolio";
+import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Subscribe from "./components/suscribe";
 import Footer from "./components/Footer";
